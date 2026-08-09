siimon/prom-client

perf: significantly improve the memory usage of histogram

lib/histogram.js
@@ -24,7 +24,7 @@ class Histogram extends Metric {
 		this.type = 'histogram';
 		this.defaultLabels = {};
 		this.defaultExemplarLabelSet = {};
-		this.enableExemplars = false;
+		this.enableExemplars = config.enableExemplars;
 
 		for (const label of this.labelNames) {
 			if (label === 'le') {
@@ -38,19 +38,10 @@ class Histogram extends Metric {
 			return acc;
 		}, {});
 
-		if (config.enableExemplars) {
-			this.enableExemplars = true;
-			this.bucketExemplars = this.upperBounds.reduce((acc, upperBound) => {
-				acc[upperBound] = null;
-				return acc;
-			}, {});
-			Object.freeze(this.bucketExemplars);
-			this.observe = this.observeWithExemplar;
-		} else {
-			this.observe = this.observeWithoutExemplar;
-		}
+		this.observe = this.enableExemplars
+			? this.observeWithExemplar
+			: this.observeWithoutExemplar;
 
-		Object.freeze(this.bucketValues);
 		Object.freeze(this.upperBounds);
 
 		if (this.labelNames.length === 0) {
@@ -58,7 +49,7 @@ class Histogram extends Metric {
 				[hashObject({})]: createBaseValues(
 					{},
 					this.bucketValues,
-					this.bucketExemplars,
+					this.enableExemplars,
 				),
 			};
 		}
@@ -79,14 +70,15 @@ class Histogram extends Metric {
 		value,
 		exemplarLabels = this.defaultExemplarLabelSet,
 	} = {}) {
-		observe.call(this, labels === 0 ? 0 : labels || {})(value);
-		this.updateExemplar(labels, value, exemplarLabels);
+		const valueFromMap = observe.call(
+			this,
+			labels === 0 ? 0 : labels || {},
+		)(value);
+		this.updateExemplar(valueFromMap, value, exemplarLabels);
 	}
 
-	updateExemplar(labels, value, exemplarLabels) {
-		const hash = hashObject(labels, this.sortedLabelNames);
+	updateExemplar({ bucketExemplars }, value, exemplarLabels) {
 		const bound = findBound(this.upperBounds, value);
-		const { bucketExemplars } = this.hashMap[hash];
 		let exemplar = bucketExemplars[bound];
 		if (!isObject(exemplar)) {
 			exemplar = new Exemplar();
@@ -137,7 +129,7 @@ class Histogram extends Metric {
 		this.hashMap[hash] = createBaseValues(
 			labels,
 			this.bucketValues,
-			this.bucketExemplars,
+			this.enableExemplars,
 		);
 	}
 
@@ -244,8 +236,9 @@ function observe(labels) {
 			valueFromMap = createBaseValues(
 				labelValuePair.labels,
 				this.bucketValues,
-				this.bucketExemplars,
+				this.enableExemplars,
 			);
+			this.hashMap[hash] = valueFromMap;
 		}
 
 		const b = findBound(this.upperBounds, labelValuePair.value);
@@ -253,23 +246,23 @@ function observe(labels) {
 		valueFromMap.sum += labelValuePair.value;
 		valueFromMap.count += 1;
 
-		if (Object.prototype.hasOwnProperty.call(valueFromMap.bucketValues, b)) {
+		if (typeof valueFromMap.bucketValues[b] === 'number') {
 			valueFromMap.bucketValues[b] += 1;
 		}
 
-		this.hashMap[hash] = valueFromMap;
+		return valueFromMap;
 	};
 }
 
-function createBaseValues(labels, bucketValues, bucketExemplars) {
+function createBaseValues(labels, bucketValues, enableExemplars) {
 	const result = {
 		labels,
-		bucketValues: { ...bucketValues },
+		bucketValues: Object.create(bucketValues),
 		sum: 0,
 		count: 0,
 	};
-	if (bucketExemplars) {
-		result.bucketExemplars = { ...bucketExemplars };
+	if (enableExemplars) {
+		result.bucketExemplars = {};
 	}
 	return result;
 }
@@ -290,16 +283,15 @@ function extractBucketValuesForExport(histogram) {
 	const name = `${histogram.name}_bucket`;
 	return bucketData => {
 		let acc = 0;
+		const { labels, bucketValues, bucketExemplars } = bucketData;
 		const buckets = histogram.upperBounds.map(upperBound => {
-			acc += bucketData.bucketValues[upperBound];
+			acc += bucketValues[upperBound];
 			return setValuePair(
 				{ le: upperBound },
 				acc,
 				name,
-				bucketData.bucketExemplars
-					? bucketData.bucketExemplars[upperBound]
-					: null,
-				bucketData.labels,
+				(bucketExemplars && bucketExemplars[upperBound]) || null,
+				labels,
 			);
 		});
 		return { buckets, data: bucketData };

prometheus/client_js

lib/histogram.js
@@ -37,7 +37,7 @@ class Histogram extends Metric {
 		this.type = 'histogram';
 		this.defaultLabels = {};
 		this.defaultExemplarLabelSet = {};
-		this.enableExemplars = false;
+		this.enableExemplars = config.enableExemplars;
 
 		for (const label of this.labelNames) {
 			if (label === 'le') {
@@ -51,26 +51,17 @@ class Histogram extends Metric {
 			return acc;
 		}, {});
 
-		if (config.enableExemplars) {
-			this.enableExemplars = true;
-			this.bucketExemplars = this.upperBounds.reduce((acc, upperBound) => {
-				acc[upperBound] = null;
-				return acc;
-			}, {});
-			Object.freeze(this.bucketExemplars);
-			this.observe = this.observeWithExemplar;
-		} else {
-			this.observe = this.observeWithoutExemplar;
-		}
+		this.observe = this.enableExemplars
+			? this.observeWithExemplar
+			: this.observeWithoutExemplar;
 
-		Object.freeze(this.bucketValues);
 		Object.freeze(this.upperBounds);
 
 		if (this.labelNames.length === 0) {
 			this.store = new LabelMap(this.labelNames);
 			this.store.merge(
 				{},
-				createBaseValues({}, this.bucketValues, this.bucketExemplars),
+				createBaseValues({}, this.bucketValues, this.enableExemplars),
 			);
 		}
 	}
@@ -90,14 +81,13 @@ class Histogram extends Metric {
 		value,
 		exemplarLabels = this.defaultExemplarLabelSet,
 	} = {}) {
-		observe(this, labels === 0 ? 0 : labels || {}, value);
-		this.updateExemplar(labels, value, exemplarLabels);
+		const valueFromMap = observe(this, labels === 0 ? 0 : labels || {}, value);
+		this.updateExemplar(valueFromMap, value, exemplarLabels);
 	}
 
-	updateExemplar(labels, value, exemplarLabels) {
+	updateExemplar({ bucketExemplars }, value, exemplarLabels) {
 		if (isEmpty(exemplarLabels)) return;
 		const bound = findBound(this.upperBounds, value);
-		const { bucketExemplars } = this.store.entry(labels);
 		let exemplar = bucketExemplars[bound];
 		if (!isObject(exemplar)) {
 			exemplar = new Exemplar();
@@ -147,7 +137,7 @@ class Histogram extends Metric {
 		this.store.validate(labels);
 		this.store.merge(
 			labels,
-			createBaseValues(labels, this.bucketValues, this.bucketExemplars),
+			createBaseValues(labels, this.bucketValues, this.enableExemplars),
 		);
 	}
 
@@ -173,7 +163,9 @@ class Histogram extends Metric {
 		const labels = getLabels(this.labelNames, args);
 		this.store.validate(labels);
 		return {
-			observe: value => observe(this, labels, value),
+			observe: value => {
+				observe(this, labels, value);
+			},
 			startTimer: () => startTimer(this, labels),
 		};
 	}
@@ -233,6 +225,7 @@ function findBound(upperBounds, value) {
  * @param {Histogram} histogram
  * @param {object} labels - Object with labels where key is the label key and value is label value. Can only be one level deep
  * @param {number} value - Value to observe in the histogram
+ * @returns {object} bucket data
  */
 function observe(histogram, labels, value) {
 	const labelValuePair = convertLabelsAndValues(labels, value);
@@ -251,7 +244,7 @@ function observe(histogram, labels, value) {
 			createBaseValues(
 				labelValuePair.labels,
 				histogram.bucketValues,
-				histogram.bucketExemplars,
+				histogram.enableExemplars,
 			),
 		);
 	}
@@ -261,20 +254,22 @@ function observe(histogram, labels, value) {
 	entry.sum += labelValuePair.value;
 	entry.count += 1;
 
-	if (Object.hasOwn(entry.bucketValues, b)) {
+	if (typeof entry.bucketValues[b] === 'number') {
 		entry.bucketValues[b] += 1;
 	}
+
+	return entry;
 }
 
-function createBaseValues(labels, bucketValues, bucketExemplars) {
+function createBaseValues(labels, bucketValues, enableExemplars) {
 	const result = {
 		labels,
-		bucketValues: { ...bucketValues },
+		bucketValues: Object.create(bucketValues),
 		sum: 0,
 		count: 0,
 	};
-	if (bucketExemplars) {
-		result.bucketExemplars = { ...bucketExemplars };
+	if (enableExemplars) {
+		result.bucketExemplars = {};
 	}
 	return result;
 }
@@ -295,16 +290,15 @@ function extractBucketValuesForExport(histogram) {
 	const name = `${histogram.name}_bucket`;
 	return bucketData => {
 		let acc = 0;
+		const { labels, bucketValues, bucketExemplars } = bucketData;
 		const buckets = histogram.upperBounds.map(upperBound => {
-			acc += bucketData.bucketValues[upperBound];
+			acc += bucketValues[upperBound];
 			return setValuePair(
 				{ le: upperBound },
 				acc,
 				name,
-				bucketData.bucketExemplars
-					? bucketData.bucketExemplars[upperBound]
-					: null,
-				bucketData.labels,
+				bucketExemplars?.[upperBound] ?? null,
+				labels,
 			);
 		});
 		return { buckets, data: bucketData };