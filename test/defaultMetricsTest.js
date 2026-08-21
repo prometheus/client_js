6064bd54dde6216f83023c033c075c0825cde430
Jon Suguiyama <jon.suguiyama@gmail.com>
2026-08-21 12:24:03 -0300

    Alphabetize default metrics and fix heapSizeAndUsed collect order
    Register default metric modules in alphabetical order of their metric
    name in defaultMetrics.js. Move the shared collect() in
    heapSizeAndUsed from the total gauge to the external gauge, since
    nodejs_external_memory_bytes now sorts first. Add a test asserting
    module registration order is alphabetical.

--- test/defaultMetricsTest.js
+++ test/defaultMetricsTest.js
@@ -115,6 +115,14 @@ describe.each([
 		});
 	});
 
+	it('should register default metric modules in alphabetical order of their metric names', () => {
+		const moduleKeys = collectDefaultMetrics.metricsList;
+		const firstNames = moduleKeys.map(
+			key => require(`../lib/metrics/${key}`).metricNames[0],
+		);
+		expect(firstNames).toEqual([...firstNames].sort());
+	});
+
 	describe('custom registry', () => {
 		it('should allow to register metrics to custom registry', async () => {
 			const registry = new Registry(regType);
